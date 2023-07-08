f1425bfa2f5ac12c334cb19edff39ead7f4d80e7
Ghulam Dastgeer Mughal <ghulam.dastgeer374@gmail.com>
2023-07-08 15:59:16 +0500

    we added server js

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,14 @@ const app = express();
 //     credentials: true,
 //   })
 // );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(
   cors({
     origin: true,
