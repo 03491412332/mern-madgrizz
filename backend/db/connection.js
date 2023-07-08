81c664bdf505984aeffbc7f374ae7e0bf754607d
Ghulam Dastgeer Mughal <ghulam.dastgeer374@gmail.com>
2023-07-08 20:36:29 +0500

    databse string updated

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 exports.connection = async () => {
   mongoose.connect(
-    "mongodb+srv://medgrizz:ilTeQttmmcx6e80E@cluster0.aqusjbt.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://ghulamdastgeer374:eedDpiMvMBbrUb0k@cluster0.utdc9kh.mongodb.net/?retryWrites=true&w=majority"
   );
   //mongoose.connect("mongodb://localhost:27017/MedGrizz");
+  //"mongodb+srv://medgrizz:ilTeQttmmcx6e80E@cluster0.aqusjbt.mongodb.net/?retryWrites=true&w=majority"
+  //ghulamdastgeer374
+  //eedDpiMvMBbrUb0k
+  //mongodb+srv://ghulamdastgeer374:eedDpiMvMBbrUb0k@cluster0.utdc9kh.mongodb.net/?retryWrites=true&w=majority
 };
