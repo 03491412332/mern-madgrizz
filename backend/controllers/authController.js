9a2369e1566e84acfd8d0b2776eb2c7b5ff84060
Ghulam Dastgeer Mughal <ghulam.dastgeer374@gmail.com>
2023-07-08 16:55:09 +0500

    hi

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -109,7 +109,7 @@ const signin = catchAsync(async (req, res, next) => {
   }
   // 3) if everything is ok send the token
   const token = signToken(user._id);
-  //res.header("Access-Control-Allow-Origin", true);
+  res.header("Access-Control-Allow-Origin", true);
   return res.status(200).json({
     status: "success",
     data: {
