39e91f7ca8723c7a4909a66f024f40813098f4be
Ghulam Dastgeer Mughal <ghulam.dastgeer374@gmail.com>
2023-07-08 16:41:34 +0500

    hi

--- backend/server.js
+++ backend/server.js
@@ -51,14 +51,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 // app.use(express.static(__dirname, "public"));
 app.get("/", (req, res, next) => {
   return res.send("Hi from backend");
@@ -77,6 +69,14 @@ app.all("*", (req, res, next) => {
   next(new AppError(`we can not find ${req.originalUrl} on this server `, 400));
 });
 app.use(globalErrorHandler);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 const PORT = 4000;
 app.listen(PORT, () => {
   connection()
