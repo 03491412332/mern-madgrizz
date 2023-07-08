1e8a17c19d3eb01ee945e84bab497863ad3a253a
Ghulam Dastgeer Mughal <ghulam.dastgeer374@gmail.com>
2023-07-08 17:17:50 +0500

    hi

--- backend/server.js
+++ backend/server.js
@@ -51,15 +51,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-  res.header(
+  // res.header(
+  //   "Access-Control-Allow-Origin",
+  //   "https://medgrizz-frontend.vercel.app"
+  // );
+  // res.header(
+  //   "Access-Control-Allow-Headers",
+  //   "Origin, X-Requested-With, Content-Type, Accept"
+  // );
+  res.setHeader(
     "Access-Control-Allow-Origin",
     "https://medgrizz-frontend.vercel.app"
   );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Credentials", true);
   next();
+  console.log("response headers", res);
 });
 // app.use(express.static(__dirname, "public"));
 app.get("/", (req, res, next) => {
